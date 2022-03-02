886a08f20fa03e9792d15d4ce818649fd3da6c2c
abhinavbhardwaj <abhinavbhardwaj68@gmail.com>
2022-03-02 21:19:03 +0530

    Created a method to print longest substring in the given "n" strings.

--- src/longestCommonSubstring/LongestCommonSubstring.java
+++ src/longestCommonSubstring/LongestCommonSubstring.java
@@ -1,5 +1,8 @@
 package longestCommonSubstring;
 
+
+import java.util.Scanner;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Abhinav Bhardwaj
@@ -8,4 +11,50 @@ package longestCommonSubstring;
  */
 
 public class LongestCommonSubstring {
+
+    static String LCS(String [] s) {
+        String str = s[0];
+        int length = str.length();
+        String result = "";
+
+        for (int i = 0; i < length; i++) {
+
+            for (int j = i + 1; j <= length; j++) {
+
+                String stem = str.substring(i, j);
+                int k = 1;
+
+                for (k = 1; k < s.length; k++) {
+                    if (!s[k].contains(stem)) {
+                        break;
+                    }
+                }
+                if (k == s.length && result.length() < stem.length()) {
+                    result = stem;
+                }
+
+            }
+
+        }
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String S [] = new String[n];
+
+        for(int index = 0; index < n; index++) {
+            S[index] = sc.next();
+        }
+
+
+        String sub = LCS(S);
+
+        System.out.println(sub);
+
+    }
 }
